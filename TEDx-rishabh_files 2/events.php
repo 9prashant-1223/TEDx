614388d87024a0f698af750c48b29888b4c60719
Ankit Raj <mrankitraj@gmail.com>
2017-08-30 23:05:18 +0530

    Add files via upload

---
+++ TEDx-rishabh_files 2/events.php
@@ -0,0 +1,121 @@
+<?php include('mainframe.php'); ?>
+
+	<div class="navbar navbar-default header">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5 headwer_logo">
+					<b>TedX - IIT Roorkee</b>
+				</div>
+
+				<div class="col-md-7">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+					</button>
+
+			        <div class = "collapse navbar-collapse navHeaderCollapse">
+			           	<ul class=" header_links">
+				            <li><a href="main.php">HOME</a></li>
+				            <li><a href="about.php">ABOUT</a></li>
+				            <li><a href="#">TALKS</a></li>
+							<li class="active"><a href="events.php">EVENTS</a></li>
+							<li><a href="#">SPONSORS</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-xs-8 col-xs-offset-1 events_tag">
+    			EVENTS
+                <img class="red_circle1">
+    		</div>
+    		<div class="col-xs-6">
+    			
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-xs-9 col-xs-offset-1 mustering_tag">
+    			MUSTERING MUSES
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="text-justify col-xs-10 col-xs-offset-1 mustering_content">
+    			TEDxIITRoorkee ‘17 was held on 5th March in MAC Auditorium. The theme of the event was ‘Mustering Muses’. The Muses were invoked by artists to serve as sources of inspiration for their work. Indeed, inspiration is what fuels our actions and provides a sense of purpose. TED as a society believes passionately in the power of an individual and his ideas to change the world. This event aimed to unleash our untapped potential and to summon the very source of inspiration from within each of us.
+                The TEDx event was adorned by inspiring talks from speakers like Anshu Mor (Standup Comedian), Kashyap Deorah (Entrepreneur), Shushruthi Krishna (Femina Miss India 2016,Model & Architect), Vivek Agnihotri (Bollywood Director), Kiran Manral (Renowned Author).
+                The event was attended by a worthy audience of 200 people including students and professors. The talks were separated by interactive sessions and refreshment breaks where the great minds interacted with each other and the audience.
+
+    		</div>
+    	</div>
+
+    	<div class="row">
+    		<div class="col-xs-offset-1 col-xs-10">
+    			<div class="row" style="margin-top: 5vw">
+    				<div class="col-xs-4 col-sm-3">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                            <img class="events_video"  src="img/img1.png">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                        </div>
+                    </div>
+    				<div class="col-xs-4 col-sm-3">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                            <img class="events_video"  src="img/img1.png">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                        </div>
+                    </div>
+    				<div class="col-xs-4 col-sm-3">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                            <img class="events_video"  src="img/img1.png">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                        </div>
+                    </div>
+    				<div class="col-xs-4 col-sm-3">
+    					<img src="img/img1.png" class="events_video">
+    				</div>
+    				<div class="col-xs-4 col-sm-3">
+    					<img src="img/img1.png" class="events_video">
+    				</div>
+    				<div class="col-xs-4 col-sm-3">
+    					<img src="img/img1.png" class="events_video">
+    				</div>
+    				<div class="col-xs-4 col-sm-3">
+    					<img src="img/img1.png" class="events_video">
+    				</div>
+    				<div class="col-xs-4 col-sm-3">
+    					<img src="img/img1.png" class="events_video">
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+
+
+
+<div class="modal fade" id="img1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog event_img_modal" role="document">
+    <div class="modal-contentevent_img_modal">
+      
+      <div class="modal-body">
+        <img src="img/img1.png" class="events_video_modal">
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+    </body>
+    </html>
+
