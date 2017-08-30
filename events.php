fe087a7276b08db3a01ae75cc7921b47e9260f42
Ankit Raj <mrankitraj@gmail.com>
2017-08-31 00:55:08 +0530

    Add files via upload

--- events.php
+++ events.php
@@ -1,10 +1,10 @@
 <?php include('mainframe.php'); ?>
 
-	<div class="navbar navbar-default header">
+<!--	<div class="navbar navbar-default header">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-5 headwer_logo">
-					<b>TedX - IIT Roorkee</b>
+					<b><span style="color:#000000;">TedX - IIT Roorkee</span></b>
 				</div>
 
 				<div class="col-md-7">
@@ -16,7 +16,7 @@
 					</button>
 
 			        <div class = "collapse navbar-collapse navHeaderCollapse">
-			           	<ul class=" header_links">
+			           	<ul class="header_links">
 				            <li><a href="main.php">HOME</a></li>
 				            <li><a href="about.php">ABOUT</a></li>
 				            <li><a href="#">TALKS</a></li>
@@ -27,7 +27,41 @@
 				</div>
 			</div>
         </div>
+    </div>-->
+
+    <div class="navbar-default">
+        <!-- <div class="container-fluid"> -->
+            <div class="row header_box_shadow">
+              <!--   <div class="col-md-5 header_logo">
+                    <b>TedX - IIT Roorkee</b>
+                </div>
+ -->            <div class="col-sm-5">
+                            <img src="tednew.png" class="img-responsive" style="width: 14.72vw; margin-left: 6.875vw; margin-top: 5vh; margin-bottom: 5vh;">
+                </div>
+
+                <div class="col-md-7">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <div class = "collapse navbar-collapse navHeaderCollapse">
+                        <ul class=" header_links">
+                            <li><a href="tedx.htm">HOME</a></li>
+                            <li><a href="about.php">ABOUT</a></li>
+                            <li><a href="#" style="color: #ff2b06;">TALKS</a></li>
+                            <li><a href="events.php">EVENTS</a></li>
+                            <li><a href="sponsors.htm">SPONSORS</a></li>
+                            <li><a href="#" class="linktop">FAQ</a></li>
+                            <li><a href="#" class="linktop_register"> REGISTER</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
     </div>
+
 
     <div class="container-fluid">
     	<div class="row">
@@ -40,12 +74,56 @@
     		</div>
     	</div>
     	<div class="row">
-    		<div class="col-xs-9 col-xs-offset-1 mustering_tag">
-    			MUSTERING MUSES
+    		<div class="col-xs-9 col-md-4 col-xs-offset-1 mustering_tag" id="mustering_tag">
+    			<span class="tedxrk" style="color:red;">TEDx IIT ROORKEE</span>
     		</div>
+            <div class="col-xs-12 col-md-6 col-xs-offset-2 calendar_calendar">
+                <div class="calendar_border" style="border:2px solid rgba(0, 0, 0, 0.1);border-radius:1vw;height:8.72vh;margin-top:6.5vh;margin-left:-8vw;">
+                <div style="padding-left:3vw;padding-top:1.6vh;padding-bottom:5vh;">
+                   
+                        
+                                  <div class="col-md-1">
+                                  <a href="#" class="next1" style="visibility:hidden;">
+                                    <i class="fa fa-caret-left arrow1" aria-hidden="true"></i>
+                                  </a>
+                                  </div>
+                            <div class="calendar1 col-md-10" style="margin-left:-2vw;">
+                            <a href="#" class="year" style=" width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2017</a>
+                            <a href="#" class="year" style="margin-left:2vw; width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2016</a>
+                            <a href="#" class="year" style=" margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2015</a>
+                            <a href="#" class="year" style="margin-left:2vw; width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2014</a>
+                            <a href="#" class="year" style="margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2013</a>
+                            <a href="#" class="year" style="margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2012</a>
+                            
+                            </div>
+                            <div class="calendar2 col-md-10 cal2" style="display:none;">
+                                <a href="#" class="year" style=" width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2011</a>
+                            <a href="#" class="year" style="margin-left:2vw; width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2010</a>
+                            <a href="#" class="year" style=" margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2009</a>
+                            <a href="#" class="year" style="margin-left:2vw; width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2008</a>
+                            <a href="#" class="year" style=" margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2007</a>
+                            <a href="#" class="year" style=" margin-left:2vw;width: 58px;height: 20px;font-family: Lato;text-align: left;color: #ff2b06;color: var(--red-orange);opacity:0.32;">2006</a>
+                            </div>
+                                 <div class="col-md-1">
+                                 <a href="#" class="next2" >
+                                    <i class="fa fa-caret-right" aria-hidden="true" style="color:black;opacity:0.32;font-size:3vw;position:relative;margin-top:-2.4vh;margin-left:-2vw;"></i>
+                                 </a>
+                                 </div>
+                        
+                            
+                   
+                    
+                </div>
+                    
+                </div>
+            </div>
+            
     	</div>
-    	<div class="row">
-    		<div class="text-justify col-xs-10 col-xs-offset-1 mustering_content">
+    	<div class="row" style="margin">
+            <div class="col-xs-9 col-xs-offset-1 mustering_tag" id="mustering_tag1">
+                <span  id="symphonic">SYMPHONIC CHAOS</span>
+            </div>
+    		<div class="text-justify col-xs-10 col-xs-offset-1 mustering_content" style="margin-top:3vh;">
     			TEDxIITRoorkee ‘17 was held on 5th March in MAC Auditorium. The theme of the event was ‘Mustering Muses’. The Muses were invoked by artists to serve as sources of inspiration for their work. Indeed, inspiration is what fuels our actions and provides a sense of purpose. TED as a society believes passionately in the power of an individual and his ideas to change the world. This event aimed to unleash our untapped potential and to summon the very source of inspiration from within each of us.
                 The TEDx event was adorned by inspiring talks from speakers like Anshu Mor (Standup Comedian), Kashyap Deorah (Entrepreneur), Shushruthi Krishna (Femina Miss India 2016,Model & Architect), Vivek Agnihotri (Bollywood Director), Kiran Manral (Renowned Author).
                 The event was attended by a worthy audience of 200 people including students and professors. The talks were separated by interactive sessions and refreshment breaks where the great minds interacted with each other and the audience.
@@ -55,8 +133,16 @@
 
     	<div class="row">
     		<div class="col-xs-offset-1 col-xs-10">
-    			<div class="row" style="margin-top: 5vw">
-    				<div class="col-xs-4 col-sm-3">
+    			<div class="row" style="margin-top:8.8vh">
+    				<div class="col-xs-6 col-sm-3">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                            <img class="events_video"  src="img/img1.png" >
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                        </div>
+                    </div>
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
                         <div class="image_hover" data-toggle="modal" data-target="#img1">
                             <img class="events_video"  src="img/img1.png">
                             <div class="middle">
@@ -64,7 +150,7 @@
                             </div>
                         </div>
                     </div>
-    				<div class="col-xs-4 col-sm-3">
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
                         <div class="image_hover" data-toggle="modal" data-target="#img1">
                             <img class="events_video"  src="img/img1.png">
                             <div class="middle">
@@ -72,34 +158,138 @@
                             </div>
                         </div>
                     </div>
-    				<div class="col-xs-4 col-sm-3">
-                        <div class="image_hover" data-toggle="modal" data-target="#img1">
-                            <img class="events_video"  src="img/img1.png">
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+    					<div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
                             <div class="middle">
                                 <img src="img/zin.png" class="zout-icon">
                             </div>
-                        </div>
+    				     </div>
                     </div>
-    				<div class="col-xs-4 col-sm-3">
-    					<img src="img/img1.png" class="events_video">
-    				</div>
-    				<div class="col-xs-4 col-sm-3">
-    					<img src="img/img1.png" class="events_video">
-    				</div>
-    				<div class="col-xs-4 col-sm-3">
-    					<img src="img/img1.png" class="events_video">
-    				</div>
-    				<div class="col-xs-4 col-sm-3">
-    					<img src="img/img1.png" class="events_video">
-    				</div>
-    				<div class="col-xs-4 col-sm-3">
-    					<img src="img/img1.png" class="events_video">
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </div>
+                </div>
 
+
+                    
+
+                <div class="row" style="margin-top:0vw;">
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+    				<div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+    			</div>
+
+                <div class="row" style="margin-top:0vw;">
+                    <div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-3" style="margin-left:0vw;">
+                        <div class="image_hover" data-toggle="modal" data-target="#img1">
+                        <img src="img/img1.png" class="events_video">
+                            <div class="middle">
+                                <img src="img/zin.png" class="zout-icon">
+                            </div>
+                         </div>
+                    </div>
+                
+
+               
+
+
+
+
+
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-8 events_tag">
+                        PARTNERS
+                        <img class="red_circle2">
+                    </div>
+                    <div class="col-xs-4">
+                        
+                    </div>
+                </div>
+
+                <div class="row button_spons">
+                    <div class="col-xs-12 col-md-6 events_button">
+                      <input type="button" class="button" value="SHOW ALL PARTNERS">
+                    </div>
+
+                    <div class="col-xs-12 col-md-6" style="margin-top:-6.7vh;">
+                      <div class="row sharethispage"><span class="shrethispage" style="font-size:30px;">SHARE THIS PAGE</span><br/></div>
+                          <div class="row ">
+                          <a href="#" class="fa fa-facebook sclicons"></a>
+                          <a href="#" class="fa fa-twitter sclicons"></a>
+                          <a href="#" class="fa fa-linkedin sclicons"></a>
+                          </div>
+                    </div>
+                </div>
+
+
+
+             
+
+              </div>
+
+
+
+
+
+
+
+
+
+
+        
 
 
 
@@ -114,6 +304,33 @@
     </div>
   </div>
 </div>
+
+<script>
+
+
+
+
+$(document).ready(function(){
+     
+     $(".next2").click(function(){
+        $(".calendar1").css("display","none");
+        $(".calendar2").css("display","block");
+        $(".next2").css("visibility","hidden");
+        $(".next1").css("visibility","visible");
+        
+        });
+     $(".next1").click(function(){
+            $(".calendar1").css("display","block");
+            $(".calendar2").css("display","none");
+            $(".next2").css("visibility","visible");
+            $(".next1").css("visibility","hidden");
+
+    });
+});
+
+      
+
+</script>
 
 
     </body>
--- mainframe.php
+++ mainframe.php
@@ -16,15 +16,9 @@
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-
-    <link rel="stylesheet" type="text/css" href="css/main9.css">
-    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    
+    <script type="text/javascript" src="js/main.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-   
+    <link rel="stylesheet" type="text/css" href="css/main9.css">
   
 
 </head>
