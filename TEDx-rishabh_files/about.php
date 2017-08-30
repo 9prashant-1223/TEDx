701bd60dcd562df6e3678ac95229a7f0e4b31c5d
Ankit Raj <mrankitraj@gmail.com>
2017-08-30 22:47:01 +0530

    Add files via upload

---
+++ TEDx-rishabh_files/about.php
@@ -0,0 +1,210 @@
+
+
+<?php include('mainframe.php'); ?>
+
+
+	<div class="navbar navbar-default header">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5 headwer_logo">
+					<b>TedX - IIT Roorkee</b>
+				</div>
+
+				<div class="col-md-7">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+					</button>
+
+			        <div class = "collapse navbar-collapse navHeaderCollapse">
+			           	<ul class=" header_links">
+				            <li><a href="main.php">HOME</a></li>
+				            <li class="active"><a href="about.php">ABOUT</a></li>
+				            <li><a href="#">TALKS</a></li>
+							<li><a href="events.php">EVENTS</a></li>
+							<li><a href="#">SPONSORS</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-xs-8 col-xs-offset-1 about_us_tag">
+    			ABOUT US
+    			<img class="red_circle1">
+    		</div>
+    	</div>
+    	<div class="row" style="margin-top: 7vw">
+    		<div class="col-xs-7 col-xs-offset-1">
+    			<p class="about_us_ted_tag">TED</p>
+    			<p class="about_us_ted_content text-justify">From 1990 onward, a growing community of "TEDsters" gathered annually at the event in California State University Monterey Bay, until 2009, when it was relocated to Long Beach, California due to a substantial increase in attendees. Initially, the speakers had been drawn from the fields of expertise behind the acronym TED, but during the nineties, the roster of presenters broadened to include scientists, philosophers, musicians, religious leaders, philanthropists, and many others</p>
+    		</div>
+    		<div class="col-xs-4">
+    			<img class="about_us_speaker_img" style="margin-left: 15%; " src="img/img3.png">
+    		</div>
+    	</div>
+    </div>
+
+	<div class="container-fluid">
+		<div class="row" style="margin-top: 15vw">
+			<div class="col-xs-4">
+				<img class="about_us_speaker_img" style="margin-left: -15%" src="img/img3.png">
+			</div>
+			<div class="col-xs-7">
+				<p class="about_us_ted_tag">TED</p>
+    			<p class="about_us_ted_content text-justify">From 1990 onward, a growing community of "TEDsters" gathered annually at the event in California State University Monterey Bay, until 2009, when it was relocated to Long Beach, California due to a substantial increase in attendees. Initially, the speakers had been drawn from the fields of expertise behind the acronym TED, but during the nineties, the roster of presenters broadened to include scientists, philosophers, musicians, religious leaders, philanthropists, and many others</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+    	<div class="row" style="margin-top: 15vw">
+    		<div class="col-xs-7 col-xs-offset-1">
+    			<p class="about_us_ted_tag">TED</p>
+    			<p class="about_us_ted_content text-justify">From 1990 onward, a growing community of "TEDsters" gathered annually at the event in California State University Monterey Bay, until 2009, when it was relocated to Long Beach, California due to a substantial increase in attendees. Initially, the speakers had been drawn from the fields of expertise behind the acronym TED, but during the nineties, the roster of presenters broadened to include scientists, philosophers, musicians, religious leaders, philanthropists, and many others</p>
+    		</div>
+    		<div class="col-xs-4">
+    			<img class="about_us_speaker_img" style="margin-left: 15%; " src="img/img3.png">
+    		</div>
+    	</div>
+    </div>
+
+    <div class="container-fluid">
+    	 <div class="row">
+    		<div class="col-xs-8 col-xs-offset-1 about_us_tag" style="margin-top: 15vw">
+    			TEAM
+    			<img class="red_circle1">
+    		</div>
+    	</div>
+    	<div class="row team_row">
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+    		<div class="col-xs-4 col-sm-3">
+    			<div class="image_hover">
+					<img class="team_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/lk.png" class="linkedin-icon">
+					    </a>
+					    <a href="">	
+					    	<img src="img/fb.png" class="linkedin-icon">
+					    </a>
+					</div>
+				</div>
+    			<p class="team_name">Vivek Anand</p>
+    			<p class="team_section">Head Design</p>
+    		</div>
+
+    	</div>
+    </div>
+
+</body>
+</html>
---
+++ TEDx-rishabh_files/main.php
@@ -0,0 +1,384 @@
+
+<?php include('mainframe.php'); ?>
+	
+	<div class="navbar navbar-default header">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5 header_logo">
+					<b>TedX - IIT Roorkee</b>
+				</div>
+
+				<div class="col-md-7">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+					</button>
+
+			        <div class = "collapse navbar-collapse navHeaderCollapse">
+			           	<ul class=" header_links">
+				            <li class="active"><a href="main.php">HOME</a></li>
+				            <li><a href="about.php">ABOUT</a></li>
+				            <li><a href="events.php">TALKS</a></li>
+							<li><a href="#">EVENTS</a></li>
+							<li><a href="#">SPONSORS</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+        </div>
+        <div class="main_building_img_div">
+    	<img src="img/img6.png" class="main_building_img"></div>
+    </div>
+    </div>
+
+    
+	
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-6">
+    			
+    		</div>
+    		<div class="col-md-6">
+    			<p class="tedx_tag">TEDX IIT ROORKEE PRESENTS</p>
+    			<p class="mustering-tag"><b>SYMPHONIC</b></p>
+    			<p class="muses-tag"><b>CHAOS</b></p>
+    			<p class="main_date">14<sup>th</sup>OCT 17</p>
+    			<p class="main_location">MAC AUDITORIUM</p>
+    			<div class="mustering_circle"></div>
+    			<div class="row">
+					<input type="button" name="" class="main_register_now" value="REGISTER NOW">
+					<input type="button" name="" class="main_know_more" value="KNOW MORE">
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+<style>
+	/*.mustering_circle{
+		background-color: f24040;
+		height: 20vw;
+		width: 20vw;
+		border-radius: 20vw;
+		position: absolute;
+		z-index: -1
+	}*/
+	
+</style>
+
+	<div class="container-fluid">
+		<div class="row" style="margin-top: 20vw">
+			<div class="row  theme_tag">
+				THEME
+    			<img class="red_circle1">
+			</div>
+			<div class="row">
+				<div class="col-xs-7 col-xs-offset-1">
+				<div class="row">
+					<div class="col-xs-9 theme_name">
+						<br>Symphonic Chaos				
+					</div>
+					<div class="col-xs-6">
+						
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-11 col-xs-offset-0 text-justify theme_content">
+						<br>After graduating from business school in the spring of 1995, Sandberg worked as a management consultant for McKinsey & Company for approximately one year (1995-1996). From 1996 to 2001 she again worked for Larry Summers, who was then serving as the United States Secretary of the Treasury under President Bill Clinton. Sandberg assisted in the Treasury's work on forgiving debt in the developing world during the Asian financial crisis
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<img class="theme_img" src="img/img3.png">				
+			</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="container-fluid">
+		<div class="row speaker_tag">
+			<div class="colxs-6 col-sm-offset-1 ">
+				SPEAKERS
+				<img class="red_circle1">
+			</div>
+		</div>
+		<div class="row">
+		<ul class="nav nav-tabs tab_list" role="tablist" style="border: none;text-align: center;">
+        	<li role="presentation" class="active" style="float:none;display:inline-block;">
+        		<div style="text-align: center;" data-toggle="tab" href="#speaker1">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation"  style="float:none;display:inline-block;">
+        		<div  style="text-align: center;" data-toggle="tab" href="#speaker2">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation"  style="float:none;display:inline-block;">
+        		<div  style="text-align: center;"  data-toggle="tab" href="#speaker3">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation"  style="float:none;display:inline-block;">
+        		<div style="text-align: center;" data-toggle="tab" href="#speaker4">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation" style="float:none;display:inline-block;">
+        		<div  style="text-align: center;" data-toggle="tab" href="#speaker5">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation" class="active" style="float:none;display:inline-block;" >
+        		<div  style="text-align: center;" data-toggle="tab" href="#speaker6">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+        	<li role="presentation" style="float:none;display:inline-block;">
+        		<div style="text-align: center;" data-toggle="tab" href="#speaker7">
+					<img class="speakers_tab_img"  src="img/img2.png" >
+					<p class="speaker_tab_name">SHERYL SANDERG</p>
+				</div>
+        	</li>
+ 		</ul>
+
+        <div class="tab-content"> 
+         	<div id="speaker1" class="tab-pane fade in active">
+         		<div class="row" style="margin-top: 50px">
+					<div class="col-xs-4">
+						<img src="img/img2.png" class="speaker_img">				
+					</div>
+					<div class="col-xs-8">
+						<div class="row">
+							<div class="col-xs-8 speaker_name">
+								SHERYL SANDBERG
+								<p class="speaker_job">CEO, Facebook</p>
+							</div>
+							<div class="col-xs-6">
+								
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-11 col-xs-offset-0 text-justify speaker_content">
+								<br>After graduating from business school in the spring of 1995, Sandberg worked as a management consultant for McKinsey & Company for approximately one year (1995-1996). From 1996 to 2001 she again worked for Larry Summers, who was then serving as the United States Secretary of the Treasury under President Bill Clinton. Sandberg assisted in the Treasury's work on forgiving debt in the developing world during the Asian financial crisis
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="speaker2" class="tab-pane fade in">
+         		<div class="row" style="margin-top: 50px">
+					<div class="col-xs-4">
+						<img src="img/img1.png" class="speaker_img">				
+					</div>
+					<div class="col-xs-8">
+						<div class="row">
+							<div class="col-xs-6 speaker_name">
+								SHERYL SANDBERG
+								<p class="speaker_job">CEO, Facebook</p>
+							</div>
+							<div class="col-xs-6">
+								
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-11 col-xs-offset-0 text-justify speaker_content">
+								<br>After graduating from business school in the spring of 1995, Sandberg worked as a management consultant for McKinsey & Company for approximately one year (1995-1996). From 1996 to 2001 she again worked for Larry Summers, who was then serving as the United States Secretary of the Treasury under President Bill Clinton. Sandberg assisted in the Treasury's work on forgiving debt in the developing world during the Asian financial crisis
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="speaker3" class="tab-pane fade in">
+         		<div class="row" style="margin-top: 50px">
+					<div class="col-xs-4">
+						<img src="img/img3.png" class="speaker_img">				
+					</div>
+					<div class="col-xs-8">
+						<div class="row">
+							<div class="col-xs-6 speaker_name">
+								SHERYL SANDBERG
+								<p class="speaker_job">CEO, Facebook</p>
+							</div>
+							<div class="col-xs-6">
+								
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-11 col-xs-offset-0 text-justify speaker_content">
+								<br>After graduating from business school in the spring of 1995, Sandberg worked as a management consultant for McKinsey & Company for approximately one year (1995-1996). From 1996 to 2001 she again worked for Larry Summers, who was then serving as the United States Secretary of the Treasury under President Bill Clinton. Sandberg assisted in the Treasury's work on forgiving debt in the developing world during the Asian financial crisis
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+
+		
+	</div>
+
+
+
+<!-- 
+	<div class="container-fluid" >
+		<div class="row previous_talk_div">
+			<div class="col-sm-6 col-sm-offset-1">
+				PREVIOUS TALK
+				<img class="red_circle1">
+			</div>
+		</div>
+		<div class="row previous_talk_row">
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+		</div>
+		<div class="row previous_talk_row">
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+			<div class="col-md-4">
+				<div class="image_hover">
+					<img class="previous_talk_img"  src="img/img2.png">
+					<div class="middle">
+					    <a href="">
+					    	<img src="img/yt.png" class="youtube-icon">
+					    </a>
+					</div>
+				</div>
+				<p class="previous_talk_name">SHERYL SANDERG</p>
+				<p class="previous_talk_job">CEO, Facebook</p> 
+			</div>
+		</div>
+
+	</div>
+
+<style>
+	
+	.partners_tag{
+		font-weight: 900;
+		font-size: 3.5vw;
+		color: #000;
+	}
+	.show_all_partner{
+		background-color: red;
+		padding: 1vw 4vw;
+		color: #fff;
+		font-size: 1.3vw;
+		border:none;
+		font-weight: 400;
+		margin-top: 5vw
+	}
+	.partners_img{
+		background-color: red;
+		height: 25vw;
+		width: 25vw;
+		border-radius: 50vw;
+		margin-left: 45%;
+		margin-top: -2vw
+	}
+	.share{
+		font-size: 2vw;
+		color: #333;
+		margin-top: -5vw;
+		text-align: center;
+		font-weight: 700;
+	}
+</style>
+ -->
+	<!-- <div class="container-fluid">
+		<div class="row" style="margin-top: 10vw">
+			<div class="col-xs-6 col-xs-offset-1 partners_tag">
+				 PARTNERS
+				<div>
+				 	<input type="button" name="" value="SHOW ALL PARTNERS" class="show_all_partner">
+				</div>
+			</div>
+			<div class="col-xs-5">
+				<a href="#">
+					<div class="partners_img">
+						
+					</div>
+					<p class="share">SHARE THIS PAGE</p>
+				</a>
+			</div>
+		</div>
+	</div> -->
+
+
+
+
+
+<?php include('partners.php'); ?>
+
+<script>
+	console.log('Developed By : - Prashant Verma')
+</script>
+
+</body>
+</html>
