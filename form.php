c74b1591ab80cb2bae4ec2c14ee21cb7883d7969
9prashant-1223 <prashantverma1223@gmail.com>
2017-09-22 16:03:52 +0530

    added images

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>form</title>
+</head>
+<body>
+<form>
+	<input type="text" name="" placeholder="Name">
+	<input type="password" name="" placeholder="Password">
+	<input type="password" name="" placeholder="Confirm password">
+	<input type="date" name="" placeholder="Date of birth">
+</form>
+</body>
+</html>
